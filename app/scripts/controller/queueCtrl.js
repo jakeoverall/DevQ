fdd17b6d22a8c82da9342f6e3c5fd5bcd58d27a7
Jake Overall <joverall22@gmail.com>
2014-09-03 02:09:12 -0600

    adds login and firebase logic

---
+++ app/scripts/controller/queueCtrl.js
@@ -0,0 +1,27 @@
+﻿'use strict';
+
+var devQ = angular.module('devQ');
+
+devQ.controller('queueCtrl', [ '$scope', 'queueRef', function($scope, queueRef) {
+
+    $scope.queue = queueRef().$asArray();
+
+    $scope.enterQueue = function () {
+        var question = {};
+        question.body = $scope.message || '';
+        question.status = 'Red';
+        question.submittedBy = $scope.username || '';
+        $scope.queue.$add(question);
+    };
+
+    $scope.leaveQueue = function(question) {
+        question.status = 'Green';
+        $scope.queue.$save(question);
+    };
+
+    $scope.assigned = function(question) {
+        question.status = 'yellow';
+        question.mentor = $scope.username || '';
+        $scope.queue.$save(question);
+    };
+}]);
