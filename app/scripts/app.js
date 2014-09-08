45382d0362cc89b2cd20a5e7010a550954485fb0
eanplatter <ean@eanplatter.com>
2014-09-07 23:07:40 -0600

    remove queue.html

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -19,7 +19,7 @@ devQ.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $
         })
         .state('mentor', {
             url: '/mentor',
-            templateUrl: '/app/views/student/mentor.html',
+            templateUrl: '/app/views/mentor/mentor.html',
             controller: 'mentorCtrl'
 
         })
