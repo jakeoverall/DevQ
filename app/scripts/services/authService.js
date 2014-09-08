2d02143d0278d1075c8be3e6e0b24fb99c87a502
eanplatter <ean@eanplatter.com>
2014-09-08 01:21:14 -0600

    hosted server omg it was so cool

--- app/scripts/services/authService.js
+++ app/scripts/services/authService.js
@@ -165,7 +165,7 @@ angular.module('devQ')
               var deferred = $q.defer();
               $http({
                   method: 'POST',
-                  url: 'http://localhost:1212/pin',
+                  url: 'http://devqueueserver.herokuapp.com/pin',
                   data: {
                       pin: initPW
                   }
